598adcca3371c1616fcb88289ebcdadb402ec26b
jminsausti <jminsausti@fptxurdinaga.com>
2023-11-29 18:05:01 +0100

    Hecho

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div >
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+      <HelloWorld msg="Plantilla para la creación de APPs" />
+    </div>
+  </header>
+
+  
+</template>
+
+<style scoped>
+div{
+  margin: 50px;
+  text-align: center;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<script>
+  export default {
+      name: 'HelloWorld',
+      components: {},
+      props: {
+        msg: {
+          type: String,
+          required: true
+        }
+      },
+      data(){
+          return{
+
+          }
+      },
+      methods:{
+          
+      }
+  }
+</script>
+
+
+<template>
+  <div>
+    <h1 class="green">{{ msg }}</h1>
+    <h2>ESTÁS EN HELLOWORLD</h2>
+    <h3>
+      Cuerpo del componente HelloWorld!!    
+    </h3>
+  </div> 
+</template>
+
+<style scoped>
+div{
+  text-align: center;
+}
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
